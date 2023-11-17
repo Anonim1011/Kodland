644324de12e4aa783e198aa7a941f9fddf2e24b4
Anonim1011 <151216268+Anonim1011@users.noreply.github.com>
2023-11-17 19:31:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+slowa = {
+    "lol": "odpowiedź na coś zabawnego", 
+    "CRINGE": "coś dziwnego lub wstydliwego", 
+    "ROFL": "odpowiedź na żart", "SHEESH": " lekka dezaprobata",
+    "SHEESH": "lekka dezaprobata",
+    "CREEPY ": "straszny, złowieszczy",
+    "AGGRO": "stać się agresywnym/zły"
+}
 
+word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
+
+if word in slowa.keys():
+    print(slowa[word])
+else:
+    print("nie ma takiego slowa")
